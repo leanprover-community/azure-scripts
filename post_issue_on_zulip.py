6d95a260d698090ebe2f9f27363214e3e5647442
Rob Lewis <rob.y.lewis@gmail.com>
2020-11-01 17:20:44 -0500

    don't repost too often

--- post_issue_on_zulip.py
+++ post_issue_on_zulip.py
@@ -5,11 +5,22 @@ import zulip
 import github
 import datetime
 import random
+import re
 
 zulip_token = sys.argv[1]
 gh_token = sys.argv[2]
 
 zulip_client = zulip.Client(email="random-issue-bot@zulipchat.com", api_key=zulip_token, site="https://leanprover.zulipchat.com")
+
+def message_date(id):
+    return zulip_client.get_message_history(id)['message_history'][0]['timestamp']
+
+posted_topics = zulip_client.get_stream_topics(zulip_client.get_stream_id('triage')['stream_id'])['topics']
+print(posted_topics)
+pattern = re.compile(r'\(#(\d+)\)$')
+posted_topics = {v[0]: message_date(t['max_id']) for (t, v) in ((t, pattern.findall(t['name'])) for t in posted_topics) if len(v) > 0}
+
+print(posted_topics)
 
 gh = github.Github(login_or_token=gh_token)
 mathlib = gh.get_repo('leanprover-community/mathlib')
@@ -19,15 +30,23 @@ open_prs = []
 open_issues = []
 
 for i in open_items:
-    if i.created_at < datetime.datetime.now() - datetime.timedelta(days=14) and 'blocked-by-other-PR' not in [l.name for l in i.labels]:
+    now = datetime.datetime.now()
+    delta = datetime.timedelta(days=14)
+    min_age = now - delta
+    if i.created_at < min_age \
+         and 'blocked-by-other-PR' not in [l.name for l in i.labels] \
+             and not (i.number in posted_topics and datetime.datetime.fromtimestamp(posted_topics[i.number]) > min_age):
         if i.pull_request:
             open_prs.append(i)
         else:
             open_issues.append(i)
 
+
+quit()
+
 def post_random(select_from, kind):
     random_issue = random.choice(select_from)
-    topic = f'random {kind}: {random_issue.title} (#{random_issue.number})'
+    topic = f'{kind} #{random_issue.number}: {random_issue.title}'
 
     content = f"""
 Today I chose {kind} {random_issue.number} for discussion!
