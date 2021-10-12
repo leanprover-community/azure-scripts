3c3a0484449c32ad49aa4a4e65045ad242732522
Gabriel Ebner <gebner@gebner.org>
2021-10-12 10:13:24 +0200

    cleanup: fix uninitialized variable

--- cleanup.py
+++ cleanup.py
@@ -13,7 +13,7 @@ external_repo_info = {}
 github_token = sys.argv[2]
 
 #save non-head, non-master commits for this many seconds
-DURATION = datetime.timedelta(days=3) 
+DURATION = datetime.timedelta(days=3)
 current_time = datetime.datetime.now(datetime.timezone.utc)
 
 def is_deletable_mathlib(sha, creation_time):
@@ -25,16 +25,20 @@ def is_deletable_mathlib(sha, creation_time):
 def is_deletable_external(repo, sha, creation_time):
     if repo not in external_repo_info:
         new_cloned_repo = git.Repo.clone_from(f'https://{github_token}@github.com/leanprover-community/{repo}.git',repo)
-        external_repo_info[repo] = {'branch_heads': set([r.commit.hexsha for r in new_cloned_repo.refs]), 'master_commits': set([c.hexsha for c in new_cloned_repo.iter_commits('master')])}
-    return sha not in external_repo_info[repo]['master_commits'] or (current_time - creation_time > DURATION and sha != new_cloned_repo.rev_parse('origin/master'))
+        external_repo_info[repo] = {
+            'branch_heads': set(r.commit.hexsha for r in new_cloned_repo.refs),
+            'master_commits': set(c.hexsha for c in new_cloned_repo.iter_commits('master')),
+            'master_head': new_cloned_repo.rev_parse('origin/master'),
+        }
+    return sha not in external_repo_info[repo]['master_commits'] or \
+        (current_time - creation_time > DURATION and sha != external_repo_info[repo]['master_head'])
 
 def is_deletable(path, creation_time):
-    if '/' not in path: # this archive came from mathlib 
+    if '/' not in path: # this archive came from mathlib
         return is_deletable_mathlib(path, creation_time)
     else:
         components = path.split('/')
         return is_deletable_external(components[0], components[-1], creation_time)
-        
 
 
 # azure configuration
@@ -46,7 +50,7 @@ def get_deletable_blobs():
     blob_list = list(container_client.list_blobs())
     deletable = [blob for blob in blob_list if is_deletable(blob.name[:-7], blob.last_modified)]
 
-    print(len(deletable), 'out of', len(blob_list), 'can be deleted, so we keep', 
+    print(len(deletable), 'out of', len(blob_list), 'can be deleted, so we keep',
           len(blob_list) - len(deletable))
 
     on_master = [blob for blob in blob_list if blob.name[:-7] in mathlib_master_commits]
