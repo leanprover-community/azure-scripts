05ae6737c85da931205be6a792c1f7e224e4c3bd
adomani <adomani@gmail.com>
2025-06-18 23:09:40 +0100

    add docs for awk script

--- formatGhPrList.sh
+++ formatGhPrList.sh
@@ -4,17 +4,21 @@
 # of all the issues in the current repository.
 # Assuming that `<linkifier>#NNN` is a valid Zulip linkifier, the PRs will be clickable links.
 
+# This is the start of the linkifier, without the `#`, e.g. `blog` or `website`.
 repo="${1:-linkifier}"
 
+# The output of `gh pr list` is a 5-column, tab-separated table containing
+# ID TITLE BRANCH STATUS DATE
 gh pr list |
   awk -F$'\t' -v link="${repo}#" 'BEGIN{
+    # Set the default separator to be `|` to match an md-table formatting
     OFS="|"
+    # Print the Headers of the table
     print "", " ID ", " TITLE ", " DATE ", " STATUS ", ""
+    # Print the alignments of the table
     print "", " - ", " - ", " - ", " - ", ""
   }
-    #(NR == 1){link=""}
-    #(!(NR == 1)){link=lk}
-    # skip the 3rd field, containing the branch name
+    # Skip the 3rd field, containing the branch name, remove `T<time>` from the date entry.
     {
       date=$5
       gsub(/T.*/, "", date)
